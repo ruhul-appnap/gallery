384bf5603ef9b1fa04a9caf6650a2a382b8519f0
Ruhul <ruhul@appnap.io>
2023-11-09 13:06:43 +0600

    update footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -78,9 +78,6 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
           ))}
         </div>
       </main>
-      <footer className="p-6 text-center text-white/80 sm:p-12">
-        All Rights Reserved @2023
-      </footer>
     </>
   );
 };
